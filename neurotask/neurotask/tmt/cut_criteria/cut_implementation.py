79fffc7360e2c13c07e8825f38c29b0768a1c16b
gianluca-capelo <gianluca.capelo@despegar.com>
2025-05-14 15:47:08 -0300

    refactor

--- neurotask/neurotask/tmt/cut_criteria/cut_implementation.py
+++ neurotask/neurotask/tmt/cut_criteria/cut_implementation.py
@@ -1,17 +1,17 @@
 import logging
-from typing import Optional
+from typing import Optional, Tuple, List
 
 from neurotask.tmt.cut_criteria.cut_criteria import CutCriteria
-from neurotask.tmt.metrics.targets_touch_calculator import number_of_correct_and_incorrect_targets_touched, get_correct_and_incorrect_target_touch_intervals
-from neurotask.tmt.model.tmt_model import TMTTrial, TMTSubject
+from neurotask.tmt.metrics.targets_touch_calculator import get_correct_and_incorrect_target_touch_intervals
+from neurotask.tmt.model.tmt_model import TMTTrial, TMTSubject, TMTTarget, CursorInfo
 
 
 def cut_trial(
-    trial: TMTTrial,
-    correct_targets_minimum: Optional[int],
-    subject: TMTSubject,
-    subject_id: str,
-    cut_criteria: CutCriteria
+        trial: TMTTrial,
+        correct_targets_minimum: Optional[int],
+        subject: TMTSubject,
+        subject_id: str,
+        cut_criteria: CutCriteria
 ) -> TMTTrial:
     """
     Process the given trial based on the specified cut criteria.
@@ -38,10 +38,10 @@ def cut_trial(
 
 
 def cut_trial_at_minimum_targets(
-    correct_targets_minimum: int,
-    subject: TMTSubject,
-    subject_id: str,
-    trial: TMTTrial
+        correct_targets_minimum: int,
+        subject: TMTSubject,
+        subject_id: str,
+        trial: TMTTrial
 ) -> TMTTrial:
     """
     Cuts the trial at the point where the minimum number of correct target touches is reached.
@@ -58,7 +58,10 @@ def cut_trial_at_minimum_targets(
     Raises:
         ValueError: If the trial does not meet the required number of correct target touches.
     """
-    correct_targets_touches, _ = number_of_correct_and_incorrect_targets_touched(trial, subject.target_radius)
+
+    correct_intervals, _ = get_correct_and_incorrect_target_touch_intervals(trial, subject.target_radius)
+
+    correct_targets_touches = len(correct_intervals)
 
     if correct_targets_touches < correct_targets_minimum:
         logging.warning(
@@ -67,13 +70,13 @@ def cut_trial_at_minimum_targets(
         )
         raise ValueError(f"Trial {trial.id} has less than {correct_targets_minimum} correct targets.")
 
-    return cut_trial_at_minimum_correct_targets(trial, correct_targets_minimum, subject.target_radius)
+    return cut_trial_at_minimum_correct_targets(trial, correct_targets_minimum, correct_intervals)
 
 
 def cut_trial_at_minimum_correct_targets(
-    trial: TMTTrial,
-    correct_targets_minimum: int,
-    radius: float
+        trial: TMTTrial,
+        correct_targets_minimum: int,
+        correct_intervals: List[Tuple[TMTTarget, CursorInfo, CursorInfo]]
 ) -> TMTTrial:
     """
     Cuts the trial at the time corresponding to reaching the minimum correct target touches.
@@ -81,7 +84,7 @@ def cut_trial_at_minimum_correct_targets(
     Args:
         trial (TMTTrial): The trial to be cut.
         correct_targets_minimum (int): The required number of correct touches.
-        radius (float): The target radius used to determine correct touches.
+        correct_intervals (List[Tuple[TMTTarget, CursorInfo, CursorInfo]]): The intervals of correct touches.
 
     Returns:
         TMTTrial: The trial cut at the appropriate time.
@@ -89,24 +92,23 @@ def cut_trial_at_minimum_correct_targets(
     Raises:
         ValueError: If the trial does not contain the required number of correct target segments.
     """
-    correct_segments, _ = get_correct_and_incorrect_target_touch_intervals(trial, radius)
-    if len(correct_segments) < correct_targets_minimum:
+    if len(correct_intervals) < correct_targets_minimum:
         raise ValueError(f"Trial {trial.id} has less than {correct_targets_minimum} correct targets.")
 
     # Sort segments by the time the segment started (assumed to be the second element)
-    correct_segments.sort(key=lambda segment: segment[1].time)
+    correct_intervals.sort(key=lambda segment: segment[1].time)
 
     # Identify the cutoff segment—the one at which the required count is reached.
-    cutoff_segment = correct_segments[correct_targets_minimum - 1]
+    cutoff_segment = correct_intervals[correct_targets_minimum - 1]
     cursor_info = cutoff_segment[2]
 
     return cut_at_time(trial, cursor_info.time, correct_targets_minimum)
 
 
 def cut_at_time(
-    trial: TMTTrial,
-    cutoff_time: float,
-    correct_targets_minimum: int
+        trial: TMTTrial,
+        cutoff_time: float,
+        correct_targets_minimum: int
 ) -> TMTTrial:
     """
     Cuts the trial at the specified cutoff time.
