0b60821c6911675f63057f5142b14acae88d3dfc
gianluca-capelo <gianluca.capelo@despegar.com>
2025-05-29 19:31:04 -0300

    rename

--- neurotask/neurotask/tmt/metrics/base_metric.py
+++ neurotask/neurotask/tmt/metrics/base_metric.py
@@ -1,9 +1,21 @@
 from abc import ABC, abstractmethod
+from typing import Optional
 
 from neurotask.tmt.model.tmt_model import TMTTrial, TMTSubject, TMTTarget, CursorInfo
 
 
 class BaseMetricCalculator(ABC):
+    def __init__(self, prefix: Optional[str] = None):
+        self.prefix = prefix
+
+    def get_metric_name(self, metric_name: str) -> str:
+        """
+        Devuelve el nombre de la métrica con el prefijo si se ha definido.
+        """
+        if self.prefix:
+            return f'{self.prefix}{metric_name}'
+        return metric_name
+
     @abstractmethod
     def add_metrics(self, metrics: dict, trial: TMTTrial, subject: TMTSubject,
                     trails_between_targets: list[tuple[TMTTarget, list[CursorInfo]]], calculate_crosses: bool,
