4fbf9c0e705c8a3df2bd54187cb91bea85b9796d
gianluca-capelo <gianluca.capelo@despegar.com>
2025-11-03 10:15:22 -0300

    add first click

--- neurotask/neurotask/tmt/metrics/metrics_calculator.py
+++ neurotask/neurotask/tmt/metrics/metrics_calculator.py
@@ -56,7 +56,8 @@ def generate_rows_for_subject(subject_id: str, subject: TMTSubject, correct_targ
                 rows.append(
                     create_invalid_trial_row(
                         subject, subject_id, trial, speed_threshold,
-                        invalid_cause=InvalidCause.INVALID_MODEL
+                        invalid_cause=InvalidCause.INVALID_MODEL,
+                        error_msg="Trial is_valid function return false."
                     )
                 )
                 continue
@@ -65,7 +66,8 @@ def generate_rows_for_subject(subject_id: str, subject: TMTSubject, correct_targ
             rows.append(
                 create_invalid_trial_row(
                     subject, subject_id, trial, speed_threshold,
-                    invalid_cause=InvalidCause.UNABLE_TO_DETERMINE_START
+                    invalid_cause=InvalidCause.UNABLE_TO_DETERMINE_START,
+                    error_msg=str(e)
                 )
             )
             continue
@@ -96,7 +98,8 @@ def generate_rows_for_subject(subject_id: str, subject: TMTSubject, correct_targ
                     rows.append(
                         create_invalid_trial_row(
                             subject, subject_id, trial, speed_threshold,
-                            invalid_cause=InvalidCause.UNDER_CORRECT_TARGETS_MINIMUM
+                            invalid_cause=InvalidCause.UNDER_CORRECT_TARGETS_MINIMUM,
+                            error_msg="Not enough correct target touches"
                         )
                     )
                     continue
@@ -115,11 +118,14 @@ def generate_rows_for_subject(subject_id: str, subject: TMTSubject, correct_targ
             rows.append(valid_row)
 
         except Exception as e:
+            print(f"ERROR: Trial {trial.id} for subject {subject_id}: {e}")
+            error_msg = str(e)
             logging.exception(f"Error processing trial {trial.id} for subject {subject_id}: {e}")
             rows.append(
                 create_invalid_trial_row(
                     subject, subject_id, trial, speed_threshold,
-                    invalid_cause=InvalidCause.UNKNOWN_ERROR
+                    invalid_cause=InvalidCause.UNKNOWN_ERROR,
+                    error_msg=error_msg
                 )
             )
 
@@ -204,7 +210,8 @@ def _attempt_cut_trial(
         rows.append(
             create_invalid_trial_row(
                 subject, subject_id, trial, speed_threshold,
-                invalid_cause=InvalidCause.CUT_CRITERIA_ERROR
+                invalid_cause=InvalidCause.CUT_CRITERIA_ERROR,
+                error_msg=str(e)
             )
         )
         return None
@@ -232,7 +239,8 @@ def create_invalid_trial_row(
         subject_id: str,
         trial: TMTTrial,
         speed_threshold: float,
-        invalid_cause: InvalidCause
+        invalid_cause: InvalidCause,
+        error_msg: Optional[str] = None
 ) -> Dict[str, Any]:
     if invalid_cause == InvalidCause.INVALID_MODEL:
         if not trial.is_valid_start_configuration():
@@ -264,6 +272,9 @@ def create_invalid_trial_row(
 
     session_data: Dict[str, str] = subject.session_data if subject.session_data else {}
     invalid_trial_row.update(session_data)
+
+    if error_msg:
+        invalid_trial_row["error_message"] = error_msg
 
     return invalid_trial_row
 
