166982bddb27b1490485514b0779e6c4fd620c2a
gianluca-capelo <gianluca.capelo@despegar.com>
2025-11-07 08:05:58 -0300

    take into acount first target

--- neurotask/neurotask/tmt/metrics/targets_touched.py
+++ neurotask/neurotask/tmt/metrics/targets_touched.py
@@ -57,7 +57,6 @@ def correct_touched_targets_for_every_cursor_point(
     # Result list
     result: List[Tuple[Optional[TMTTarget], CursorInfo]] = []
 
-    # Track the next expected target (starting from index 1, since index 0 is the start)
     expected_idx = 0
 
     for touched_list, cursor_info in trail_with_targets:
