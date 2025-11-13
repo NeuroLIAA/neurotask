68e482eb407e80b397ae225f5352c9d3dd8a846d
gianluca-capelo <gianluca.capelo@despegar.com>
2025-11-13 12:59:02 -0300

    refactor

--- neurotask/neurotask/tmt/metrics/zig_zag_amplitud.py
+++ neurotask/neurotask/tmt/metrics/zig_zag_amplitud.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from neurotask.tmt.metrics.base_metric import BaseMetricCalculator
+from neurotask.tmt.metrics.intra_and_inter_target_time import get_intra_target_intervals
 from neurotask.tmt.model.tmt_model import TMTTrial, TrialType, TMTSubject, TMTTarget, CursorInfo
 
 
@@ -10,25 +11,31 @@ class ZigZagAmplitude(BaseMetricCalculator):
                     trails_between_targets: list[tuple[TMTTarget, list[CursorInfo]]], calculate_crosses: bool,
                     speed_threshold, consecutive_points, correct_intervals) -> dict:
 
+        intra_target_intervals = get_intra_target_intervals(trial, subject)
+
         # Solo aplicable a Parte B
         if trial.trial_type != TrialType.PART_B:
             metrics[self.get_metric_name('zigzag_amplitude')] = np.nan
             return metrics
 
+        # Remove first correct interval if it corresponds to the initial target
+        if intra_target_intervals and intra_target_intervals[0][0].content == '1':
+            intra_target_intervals = intra_target_intervals[1:]
+
         time_differences = []
         # Recorremos pares [número, letra]
-        for i in range(0, len(correct_intervals) - 1, 2):
+        for i in range(0, len(intra_target_intervals) - 1, 2):
             # TODO GIAN: dejar mas claro
             # esta primero letra porque los intervalos siempre tienen como target el destino
             # por ende, nunca esta el 1
-            letter_target, letter_start_cursor_info, _ = correct_intervals[i]
-            number_target, number_start_cursor_info, _ = correct_intervals[i + 1]
+            letter_target, letter_start_cursor_info, _ = intra_target_intervals[i]
+            number_target, number_start_cursor_info, _ = intra_target_intervals[i + 1]
 
             assert number_target.content.isdigit(), f"Expected number, got {number_target.content}"
             assert letter_target.content.isalpha(), f"Expected letter, got {letter_target.content}"
 
             # time_difference = tiempo de llegada a letra - tiempo de llegada a número
-            time_difference = letter_start_cursor_info.time - number_start_cursor_info.time
+            time_difference = number_start_cursor_info.time - letter_start_cursor_info.time
             time_differences.append(time_difference)
 
         # Media de las diferencias, o NaN si no hay pares completos
